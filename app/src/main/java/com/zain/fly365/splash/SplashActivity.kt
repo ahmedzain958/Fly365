449c0d0062ce06b52d0f70e14d5fba4ac8db9c57
ahmedzain958 <ahmedzain958@gmail.com>
2019-05-23 12:00:57 +0200

    feat/add application layout

---
+++ app/src/main/java/com/zain/fly365/splash/SplashActivity.kt
@@ -0,0 +1,54 @@
+package com.zain.fly365.splash
+
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.BounceInterpolator
+import com.zain.fly365.FlightSearchActivity
+import com.zain.fly365.R
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    val ANIMATION_DURATION: Long = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        // Start animation.
+        startAnimation()
+    }
+    private fun startAnimation() {
+        // Intro animation configuration.
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            textTitle.scaleX = value
+            textTitle.scaleY = value
+        }
+        valueAnimator.interpolator = BounceInterpolator()
+        valueAnimator.duration = ANIMATION_DURATION
+
+        // Set animator listener.
+        val intent = Intent(this, FlightSearchActivity::class.java)
+        valueAnimator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {}
+
+            override fun onAnimationEnd(p0: Animator?) {
+                // Navigate to FlightSearchActivity on navigation end.
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {}
+
+            override fun onAnimationStart(p0: Animator?) {}
+
+        })
+        // Start animation.
+        valueAnimator.start()
+    }
+
+}
